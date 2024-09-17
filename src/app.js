375b2126b58caa33f1aafc21d5bca5619bb6f0c7
Arpandev-code <arpan.dev2016@gmail.com>
2024-09-17 16:48:18 +0530

    playing around with multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,13 +2,44 @@ const express= require('express');
 const app = express();
 
 
+app.get('/user/:id/:name', (req,res)=>{
+    console.log(req.query);
+    console.log(req.params);
+    
+    res.send("Hello from Root");
+})
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server");
-})
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello Hello");
-})
+app.get('/user',
+    (req,res,next)=>{
+        console.log("Hello from route Handler 1");
+        
+        // res.send({userID:"1",userName:"Arpan"});
+        next();
+    },
+    (req,res,next)=>{
+        console.log("Hello from route Handler 2");
+       // res.send({userID:"2",userName:"Priyam"});
+        next()
+    },
+    (req,res,next)=>{
+        console.log("Hello from route Handler 3");
+        res.send({userID:"3",userName:"Shubham"});
+    },
+    (req,res,next)=>{
+        console.log("Hello from route Handler 4");
+        res.send({userID:"4",userName:"Arun"});
+    },
+)
+ 
+// app.use("/",(req,res)=>{//it will check for 
+//     res.send("Hello from Root");
+// })
+// app.use("/test",(req,res)=>{
+//     res.send("Hello from the server");
+// })
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello Hello Hello");
+// })
 
 app.listen(3000,()=>{
     console.log('Listening on port 3000');
