d54f82d533a467582e5983a4a198ce06f024c8b2
Arpandev-code <arpan.dev2016@gmail.com>
2024-09-26 12:49:37 +0530

    added connectionRequest api

--- src/app.js
+++ src/app.js
@@ -224,6 +224,14 @@ app.get('/profile',userAuthCheck, async(req,res)=>{
     }
     
 })
+app.get('/sendconnectionRequest',userAuthCheck, async(req,res)=>{ 
+    try {
+        const user=req.user;
+        res.status(200).send(`${user.firstName} has send a connection request`)
+    } catch (error) {
+        res.status(500).send({Error: `${error.message}`})
+    }
+  })
 
 connection()
 .then(()=>{
